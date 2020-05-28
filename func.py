651cbabb3632305fbc25725eecd211557e9b3cc6
csh-iaas-cec <51996734+csh-iaas-cec@users.noreply.github.com>
2020-05-28 18:03:04 +0530

    commit

--- func.py
+++ func.py
@@ -17,11 +17,13 @@ def copy_vol(blockStorageClient, volume_backup_id):
 
 def handler(ctx, data: io.BytesIO=None):
     signer = oci.auth.signers.get_resource_principals_signer()
+    os_client = oci.object_storage.ObjectStorageClient(config={}, signer=signer)
     blockStorageClient = oci.core.BlockstorageClient(config={}, signer=signer)
     try:
         body = json.loads(data.getvalue())
         logging.info(body)
         volume_group_backup_id = body.get("id")
+        logging.info(os_client.get_namespace().data)
         # volume_group_backup_id = "ocid1.volumegroupbackup.oc1.iad.abuwcljryruljo27yv7kvgi6jmijojglh2ktuxgqurs6gituzo66z662npta"
         # logging.info(body)
     except (Exception, ValueError) as ex:
