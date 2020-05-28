c3636b1ae9acc961d0b8c91ccbd6c7b6a90fa92f
csh-iaas-cec <51996734+csh-iaas-cec@users.noreply.github.com>
2020-05-28 15:52:30 +0530

    updated

--- func.py
+++ func.py
@@ -1,21 +1,31 @@
 import io
 import json
 import logging
+import oci
 
 from fdk import response
 
+def get_volumes(blockStorageClient, volume_group_backup_id):
+    volume_group_backup = blockStorageClient.get_volume_group_backup(volume_group_backup_id).data
+    backup_ids = volume_group_backup.volume_backup_ids
+    for backup_id in backup_ids:
+        copy_vol(blockStorageClient, backup_id)
+
+def copy_vol(blockStorageClient, volume_backup_id):
+    copy_volume_backup_details = oci.core.models.CopyVolumeBackupDetails(destination_region="us-phoenix-1")
+    blockStorageClient.copy_volume_backup(volume_backup_id, copy_volume_backup_details)
 
 def handler(ctx, data: io.BytesIO=None):
-    name = "World"
+    signer = oci.auth.signers.get_resource_principals_signer()
+    blockStorageClient = oci.core.BlockstorageClient(config={}, signer=signer)
     try:
         body = json.loads(data.getvalue())
-        name = body.get("name")
+        volume_group_backup_id = body.get("id")
     except (Exception, ValueError) as ex:
-        logging.getLogger().info('error parsing json payload: ' + str(ex))
-
-    logging.getLogger().info("Inside Python Hello World function")
+        logging.info('error parsing json payload: ' + str(ex))
+    get_volumes(blockStorageClient, volume_group_backup_id)
     return response.Response(
         ctx, response_data=json.dumps(
-            {"message": "Hello {0}".format(name)}),
+            {"message": "Hello"}),
         headers={"Content-Type": "application/json"}
     )
